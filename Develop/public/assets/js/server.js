298d881c34c4e01f6f2f9620ae01c49601e01b46
ItsEvdok <semionaevdokimov@icloud.com>
2021-05-09 23:22:23 -0400

    minute changes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../index.html"));
@@ -14,9 +15,18 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../../notes.html"));
 });
 
-// app.get("/api/notes", (req, res) => {});
+app.get("/api/notes", (req, res) => {
 
-// app.post("/api/notes", (req, res) => {});
+});
+
+app.post("/api/notes", (req, res) => {
+    const note = req.body;
+
+    console.log(note);
+
+    activeNote.push(note);
+    res.json(note);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
