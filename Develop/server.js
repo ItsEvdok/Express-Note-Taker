87d12e560bb329579d7229eced0755fac6848f71
ItsEvdok <semionaevdokimov@icloud.com>
2021-05-10 20:46:55 -0400

    changed server file location

--- Develop/server.js
+++ Develop/server.js
@@ -8,11 +8,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", (req, res) => {
